4ca359ee3ebd1e0e431bf6f5c73044ed9eb36e96
Lahiru Karunarathna <lahirukavinda365@gmail.com>
2023-08-23 00:28:49 +0530

    Basic file structure created. setup done.

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+const app = express();
+
+
+app.listen(3000 , () => {
+    console.log("Server running on port 3000.");
+})
+
+
+
+
+
+
+
+
+
+
+
